2b5e670352b706c7e617943b23b0e7d996c3a2f4
jpastorcasquero <j.pastorcasquero@gmail.com>
2023-11-18 13:11:41 +0100

    -

--- batch/module4.py
+++ batch/module4.py
@@ -1,24 +1,34 @@
+import sys
+
 from joblib import load
 from batch import functions
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def batchFour():
     print("\n############ Ejecutando Batch 4: Carga clasificador y realizar predicciones #############")
 
     file,ruta =functions.obtener_ruta_guardado_clf()
-    print (ruta+file)
 
-    # Cargar el clasificador desde el archivo
+        # Cargar el clasificador desde el archivo
     loaded_classifier = load(ruta+file)
-    '''
-    # Suponiendo que tienes nuevos datos 'X_new' para hacer predicciones
-    # Reemplaza 'X_new' con tus propios datos
-    # Debes asegurarte de que 'X_new' tenga la misma estructura que los datos utilizados para entrenar el clasificador
-
-    # Realizar predicción con los nuevos datos
-    predictions = loaded_classifier.predict("buenos dias, soy humano")
-    print(predictions)
-
-    # Las predicciones se encuentran ahora en la variable 'predictions'
-    # Puedes usar estas predicciones según sea necesario
-    '''
+    print("Clasificador "+file+ " cargado")
+    # Crear un vectorizador con las mismas configuraciones que usamos en entrenamiento
+    max_features = 2000
+    vectorizer = TfidfVectorizer(max_features=max_features, stop_words="english", ngram_range=(1, 1))
+    valores_aceptados = ['S', 'N']
+    entrada = 'a'
+    while entrada != valores_aceptados:
+        entrada = input("\n¿Desea usted comprobar un texto? S/N:\n")
+        if entrada in ['S', 's']:
+            texto=input("Ingresa el texto:")
+            texto_vectorizado = vectorizer.transform([texto])
+            # Realizar la predicción con el modelo cargado
+            prediction = loaded_classifier.predict(texto_vectorizado)
+            # Imprimir la predicción
+            print(f"La predicción para la cadena de texto es: {prediction}")
+        elif entrada in ['N', 'n']:
+            print ("Aplicacion finalizada.\nCerrandose aplicacion")
+            sys.exit()
+        else:
+            print("Seleccione una opción válida")
