dcf8c7c01a2b06d4e150bb31c5dead1b01ef82f4
secali <sergio13896@gmail.com>
2023-11-18 11:09:54 +0100

    Inicio de module3.py, clasificador

--- batch/module3.py
+++ batch/module3.py
@@ -1,6 +1,7 @@
 # import required libraries
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -15,5 +16,12 @@ def batchThree(dfHuman, dfIA):
     random_seed = 777  # set random seed for reproducibility
     id2label = {0: "h", 1: "g"}
 
-    print(dfHuman)
-    print(dfIA)
+    df_union = pd.concat([dfHuman, dfIA], axis=0)
+
+    X = df_union.drop('Type', axis=1)
+    y = df_union['Type']
+
+    # Dividir los datos en conjuntos de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
