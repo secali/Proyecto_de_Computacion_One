7ba13a7ec8a0a2eb7eee39fb41fb6ba0e653ca44
Sergio Cañete Linares <sergio13896@gmail.com>
2024-01-02 22:30:48 +0100

    Añadido el front utilizando Flask

---
+++ README.md
@@ -0,0 +1,11 @@
+La estructura del proyecto en Flask es:
+/web/
+-->/static         AQUI TENEMOS EL CSS
+--->/templates     AQUI LAS PLANTILLAS HTML
+
+Para ejecutar el frontend desarrollado con Flask, hay que lanzar en la terminal desde la raiz del proyecto:
+
+
+flask --app .\web\app.py run
+
+Se abrirá una conexión local en http://127.0.0.1:5000/
---
+++ web/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # Renderiza el archivo de plantilla index.html
+    return render_template('index.html')
+
+
+@app.route('/analizar', methods=['POST'])
+def analizar(resultado=None):
+    # Obtiene los datos del formulario
+    modelo = request.form['modelo']
+    texto = request.form['texto']
+    # Aquí puedes usar el modelo de deeplearning que quieras para analizar el texto
+    # Por ejemplo, si usas la biblioteca transformers, puedes hacer algo así:
+    # from transformers import pipeline
+    # nlp = pipeline(modelo)
+    # resultado = nlp(texto)
+    # Devuelve el resultado del análisis en el archivo de plantilla resultado.html
+    return render_template('resultado.html', resultado=resultado)
