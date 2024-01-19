4519df41d0099f9e7a53d54cee59780fc823462b
jpastorcasquero <j.pastorcasquero@gmail.com>
2024-01-19 21:38:01 +0100

    -

--- batch/module4.py
+++ batch/module4.py
@@ -11,32 +11,41 @@ def batchFour(modelo, texto):
     print("\n############ Ejecutando Batch 4: Carga clasificador y realizar predicciones #############")
 
     # cargamos el clasificador desde el archivo
-    loaded_classifier = load(functions.obtener_ruta_guardado('SaveCLF', 'clf.joblib'))
-    print("Clasificador cargado")
+    loaded_classifier_A = load(functions.obtener_ruta_guardado('SaveCLF', 'clf_A.joblib'))
+    print("Clasificador A cargado")
     # cargar el vectorizador desde un archivo
-    loaded_vectorizador = load(functions.obtener_ruta_guardado('SaveVCT', 'vct.joblib'))
-    print("Vectorizador cargado")
+    loaded_vectorizador_A = load(functions.obtener_ruta_guardado('SaveVCT', 'vct_A.joblib'))
+    print("Vectorizador A cargado")
 
-    # bucle para indicar si queremos hacer alguna predicción
-    valores_aceptados = ['S', 'N']
-    entrada = 'a'
-    while entrada != valores_aceptados:
-        entrada = input("\n¿Desea usted comprobar un texto? S/N:\n")
-        if entrada in ['S', 's']:
-            texto = input("Ingresa el texto:")
-            data = {'Text': [texto],
-                    'Label': ['f']}
-            # preparamos texto introducido
-            dfTexto = pd.DataFrame(data, index=['Fila_1'])
-            texto_vectorizado = loaded_vectorizador.transform(dfTexto["Text"])
-            # lanzamos la prediccion
-            y_pred = loaded_classifier.predict(texto_vectorizado)
-            if y_pred == 0:
-                print('El texto introducido ha sido escrito por un humano')
-            else:
-                print('El texto inntroducio ha sido generado por IA')
-        elif entrada in ['N', 'n']:
-            print("Aplicacion finalizada.\nCerrandose aplicacion")
-            sys.exit()
+    # cargamos el clasificador desde el archivo
+    loaded_classifier_B = load(functions.obtener_ruta_guardado('SaveCLF', 'clf_B.joblib'))
+    print("Clasificador B cargado")
+    # cargar el vectorizador desde un archivo
+    loaded_vectorizador_B = load(functions.obtener_ruta_guardado('SaveVCT', 'vct_B.joblib'))
+    print("Vectorizador B cargado")
+
+    if modelo == 'A':
+        texto_vectorizado = loaded_vectorizador_A.transform(texto)
+        # lanzamos la prediccion
+        y_pred = loaded_classifier_A.predict(texto_vectorizado)
+        if y_pred == 0:
+            print('El texto introducido ha sido escrito por un humano')
         else:
-            print("Seleccione una opción válida")
+            print('El texto introducio ha sido generado por IA')
+    else:
+        texto_vectorizado = loaded_vectorizador_B.transform(texto)
+        # lanzamos la prediccion
+        y_pred = loaded_classifier_B.predict(texto_vectorizado)
+        if y_pred == 0:
+            print('El texto introducido ha sido escrito por un humano')
+        elif y_pred == 1:
+            print('El texto introducio ha sido generado por ChatGPT')
+        elif y_pred == 2:
+            print('El texto introducio ha sido generado por Cohere')
+        elif y_pred == 3:
+            print('El texto introducio ha sido generado por Davinci')
+        elif y_pred == 4:
+            print('El texto introducio ha sido generado por Bloomz')
+        else:
+            print('El texto introducio ha sido generado por Dolly')
+
