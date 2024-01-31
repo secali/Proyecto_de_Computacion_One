a3938fa0bc9723de1573b034e9a11d54bcb4b185
jpastorcasquero <j.pastorcasquero@gmail.com>
2024-01-31 16:49:08 +0100

    cerrado todo y funcionando

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # import required libraries
 from batch import functions
 
-if __name__ == '__main__':
-    file_01 =functions.obtener_datos()
-
 
 def runScript():
-    file_01 = functions.obtener_datos()
+    functions.obtener_datos()
+
+
+if __name__ == '__main__':
+    runScript()
+    # file_01 = functions.obtener_datos()
